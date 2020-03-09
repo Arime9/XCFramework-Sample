172b06452e92771aac2b614963653ff6bb8767aa
Tea-and-Coffee <awo.j.xd@gmail.com>
2020-03-10 00:40:27 +0900

    add output of "otool -l"

--- build-scripts/run.sh
+++ build-scripts/run.sh
@@ -31,19 +31,6 @@ mkdir -p ./build-logs/tree/Release
 tree ./build/Release > ./build-logs/tree/Release/build.txt
 tree ./build-output/Release > ./build-logs/tree/Release/build-output.txt
 
-# Output "lipo"
-
-rm -rf ./build-logs/lipo/Release/lipo.txt
-mkdir -p ./build-logs/lipo/Release
-
-lipo -info ./build-output/Release/Sample.xcframework/ios-arm64/iOS.framework/iOS >> ./build-logs/lipo/Release/lipo.txt
-lipo -info ./build-output/Release/Sample.xcframework/ios-x86_64-simulator/iOS.framework/iOS >> ./build-logs/lipo/Release/lipo.txt
-lipo -info ./build-output/Release/Sample.xcframework/macos-x86_64/macOS.framework/macOS >> ./build-logs/lipo/Release/lipo.txt
-lipo -info ./build-output/Release/Sample.xcframework/tvos-arm64/tvOS.framework/tvOS >> ./build-logs/lipo/Release/lipo.txt
-lipo -info ./build-output/Release/Sample.xcframework/tvos-x86_64-simulator/tvOS.framework/tvOS >> ./build-logs/lipo/Release/lipo.txt
-lipo -info ./build-output/Release/Sample.xcframework/watchos-armv7k_arm64_32/watchOS.framework/watchOS >> ./build-logs/lipo/Release/lipo.txt
-lipo -info ./build-output/Release/Sample.xcframework/watchos-i386-simulator/watchOS.framework/watchOS >> ./build-logs/lipo/Release/lipo.txt
-
 # Output "file"
 
 rm -rf ./build-logs/file/Release/file.txt
@@ -56,3 +43,30 @@ file ./build-output/Release/Sample.xcframework/tvos-arm64/tvOS.framework/tvOS >>
 file ./build-output/Release/Sample.xcframework/tvos-x86_64-simulator/tvOS.framework/tvOS >> ./build-logs/file/Release/file.txt
 file ./build-output/Release/Sample.xcframework/watchos-armv7k_arm64_32/watchOS.framework/watchOS >> ./build-logs/file/Release/file.txt
 file ./build-output/Release/Sample.xcframework/watchos-i386-simulator/watchOS.framework/watchOS >> ./build-logs/file/Release/file.txt
+
+# Output "lipo"
+
+rm -rf ./build-logs/lipo/Release/info.txt
+mkdir -p ./build-logs/lipo/Release
+
+lipo -info ./build-output/Release/Sample.xcframework/ios-arm64/iOS.framework/iOS >> ./build-logs/lipo/Release/info.txt
+lipo -info ./build-output/Release/Sample.xcframework/ios-x86_64-simulator/iOS.framework/iOS >> ./build-logs/lipo/Release/info.txt
+lipo -info ./build-output/Release/Sample.xcframework/macos-x86_64/macOS.framework/macOS >> ./build-logs/lipo/Release/info.txt
+lipo -info ./build-output/Release/Sample.xcframework/tvos-arm64/tvOS.framework/tvOS >> ./build-logs/lipo/Release/info.txt
+lipo -info ./build-output/Release/Sample.xcframework/tvos-x86_64-simulator/tvOS.framework/tvOS >> ./build-logs/lipo/Release/info.txt
+lipo -info ./build-output/Release/Sample.xcframework/watchos-armv7k_arm64_32/watchOS.framework/watchOS >> ./build-logs/lipo/Release/info.txt
+lipo -info ./build-output/Release/Sample.xcframework/watchos-i386-simulator/watchOS.framework/watchOS >> ./build-logs/lipo/Release/info.txt
+
+# Output "otool"
+
+rm -rf ./build-logs/otool/Release
+mkdir -p ./build-logs/otool/Release
+
+otool -arch arm64 -l ./build-output/Release/Sample.xcframework/ios-arm64/iOS.framework/iOS > ./build-logs/otool/Release/ios-arm64-load-commands.txt
+otool -arch x86_64 -l ./build-output/Release/Sample.xcframework/ios-x86_64-simulator/iOS.framework/iOS > ./build-logs/otool/Release/ios-x86_64-simulator-load-commands.txt
+otool -arch x86_64 -l ./build-output/Release/Sample.xcframework/macos-x86_64/macOS.framework/macOS > ./build-logs/otool/Release/macos-x86_64-load-commands.txt
+otool -arch arm64 -l ./build-output/Release/Sample.xcframework/tvos-arm64/tvOS.framework/tvOS > ./build-logs/otool/Release/tvos-arm64-load-commands.txt
+otool -arch x86_64 -l ./build-output/Release/Sample.xcframework/tvos-x86_64-simulator/tvOS.framework/tvOS > ./build-logs/otool/Release/tvos-x86_64-simulator-load-commands.txt
+otool -arch armv7k -l ./build-output/Release/Sample.xcframework/watchos-armv7k_arm64_32/watchOS.framework/watchOS > ./build-logs/otool/Release/watchos-armv7k-load-commands.txt
+otool -arch arm64_32 -l ./build-output/Release/Sample.xcframework/watchos-armv7k_arm64_32/watchOS.framework/watchOS > ./build-logs/otool/Release/watchos-arm64_32-load-commands.txt
+otool -arch i386 -l ./build-output/Release/Sample.xcframework/watchos-i386-simulator/watchOS.framework/watchOS > ./build-logs/otool/Release/watchos-i386-simulator-load-commands.txt
