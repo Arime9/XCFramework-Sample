5fb905880a044c0db37ea70c5cbd420e2b94d8eb
Tea-and-Coffee <awo.j.xd@gmail.com>
2020-03-10 00:40:27 +0900

    add archive files

---
+++ build/Sample-tvOS.xcarchive/Products/Library/Frameworks/tvOS.framework/Headers/tvOS.h
@@ -0,0 +1,18 @@
+//
+//  tvOS.h
+//  tvOS
+//
+//  Created by arai on 2020/02/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for tvOS.
+FOUNDATION_EXPORT double tvOSVersionNumber;
+
+//! Project version string for tvOS.
+FOUNDATION_EXPORT const unsigned char tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <tvOS/PublicHeader.h>
+
+
