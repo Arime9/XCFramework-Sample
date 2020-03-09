e3d61c3da826bee01c0b00be956b954dec6e9e3e
Tea-and-Coffee <awo.j.xd@gmail.com>
2020-03-10 00:40:27 +0900

    update run.sh

--- build-scripts/run.sh
+++ build-scripts/run.sh
@@ -30,3 +30,29 @@ mkdir -p ./build-logs/tree/Release
 
 tree ./build/Release > ./build-logs/tree/Release/build.txt
 tree ./build-output/Release > ./build-logs/tree/Release/build-output.txt
+
+# Output "lipo"
+
+rm -rf ./build-logs/lipo/Release/lipo.txt
+mkdir -p ./build-logs/lipo/Release
+
+lipo -info ./build-output/Release/Sample.xcframework/ios-arm64/iOS.framework/iOS >> ./build-logs/lipo/Release/lipo.txt
+lipo -info ./build-output/Release/Sample.xcframework/ios-x86_64-simulator/iOS.framework/iOS >> ./build-logs/lipo/Release/lipo.txt
+lipo -info ./build-output/Release/Sample.xcframework/macos-x86_64/macOS.framework/macOS >> ./build-logs/lipo/Release/lipo.txt
+lipo -info ./build-output/Release/Sample.xcframework/tvos-arm64/tvOS.framework/tvOS >> ./build-logs/lipo/Release/lipo.txt
+lipo -info ./build-output/Release/Sample.xcframework/tvos-x86_64-simulator/tvOS.framework/tvOS >> ./build-logs/lipo/Release/lipo.txt
+lipo -info ./build-output/Release/Sample.xcframework/watchos-armv7k_arm64_32/watchOS.framework/watchOS >> ./build-logs/lipo/Release/lipo.txt
+lipo -info ./build-output/Release/Sample.xcframework/watchos-i386-simulator/watchOS.framework/watchOS >> ./build-logs/lipo/Release/lipo.txt
+
+# Output "file"
+
+rm -rf ./build-logs/file/Release/file.txt
+mkdir -p ./build-logs/file/Release
+
+file ./build-output/Release/Sample.xcframework/ios-arm64/iOS.framework/iOS >> ./build-logs/file/Release/file.txt
+file ./build-output/Release/Sample.xcframework/ios-x86_64-simulator/iOS.framework/iOS >> ./build-logs/file/Release/file.txt
+file ./build-output/Release/Sample.xcframework/macos-x86_64/macOS.framework/macOS >> ./build-logs/file/Release/file.txt
+file ./build-output/Release/Sample.xcframework/tvos-arm64/tvOS.framework/tvOS >> ./build-logs/file/Release/file.txt
+file ./build-output/Release/Sample.xcframework/tvos-x86_64-simulator/tvOS.framework/tvOS >> ./build-logs/file/Release/file.txt
+file ./build-output/Release/Sample.xcframework/watchos-armv7k_arm64_32/watchOS.framework/watchOS >> ./build-logs/file/Release/file.txt
+file ./build-output/Release/Sample.xcframework/watchos-i386-simulator/watchOS.framework/watchOS >> ./build-logs/file/Release/file.txt
